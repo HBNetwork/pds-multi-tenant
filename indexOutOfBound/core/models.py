3243082b93c170b584407028b47e1cca09aa2c81
Samuel <fg.samuel@gmail.com>
2022-03-17 20:59:51 -0300

    Create tenant

--- indexOutOfBound/core/models.py
+++ indexOutOfBound/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tenant(models.Model):
+    name = models.CharField(max_length=100)
+    database_name = models.CharField(max_length=200, unique=True)
--- indexOutOfBound/indexOutOfBound/settings.py
+++ indexOutOfBound/indexOutOfBound/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'tenant1': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'tenant1.sqlite3',
     }
 }
 
