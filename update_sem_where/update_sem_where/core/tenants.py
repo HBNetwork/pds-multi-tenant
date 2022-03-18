490235c180b2a467d579c2f025e33d8a709cfcd1
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2022-03-18 20:27:35 -0300

    Add tenants.create

--- update_sem_where/update_sem_where/core/tenants.py
+++ update_sem_where/update_sem_where/core/tenants.py
@@ -1,4 +1,17 @@
 from django.db import connection
+from django.db.utils import ProgrammingError
+
+
+class DuplicatedSchemaError(Exception):
+    pass
+
+
+def create(name):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(f"CREATE SCHEMA {name}")
+    except ProgrammingError as e:
+        raise DuplicatedSchemaError(e) from e
 
 
 def list():
--- update_sem_where/update_sem_where/core/tests/test_tenants.py
+++ update_sem_where/update_sem_where/core/tests/test_tenants.py
@@ -29,3 +29,23 @@ def test_list_multiple():
 
     assert tenants.list() == [tenant1, tenant2]
 
+
+@pytest.mark.django_db
+def test_create():
+    tenant = 'tenant'
+
+    tenants.create(tenant)
+
+    assert tenants.list() == [tenant]
+
+
+@pytest.mark.django_db
+def test_create_duplicated_raises_exception():
+    tenant = 'tenant'
+
+    tenants.create(tenant)
+
+    with pytest.raises(tenants.DuplicatedSchemaError) as error:
+        tenants.create(tenant)
+
+    assert str(error.value) == f'schema "{tenant}" already exists\n'
