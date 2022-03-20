865aeda29e6332a7364698b5187b8510dc02ff67
atmosmps <atmos.souza@luizalabs.com>
2022-03-19 21:26:04 -0300

    refactor: restrutured project

--- pycore/manage.py
+++ pycore/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'accounting_poc.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poc.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- pycore/poc/asgi.py
+++ pycore/poc/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for accounting_poc project.
+ASGI config for poc project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'accounting_poc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poc.settings')
 
 application = get_asgi_application()
