8b891628214cb595c7e003431e960c48044bc298
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2022-03-24 21:58:29 -0300

    Add tenant admin site

--- indexOutOfBound/core/admin.py
+++ indexOutOfBound/core/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 
-# Register your models here.
 from .models import Tenant
 
 admin.site.register(Tenant)
+
+
+class TenantAdminSite(AdminSite):
+    ...
+
+tenant_admin_site = TenantAdminSite(name="Tenant Admin")
